403386617eba28d4da26f6c9f511bf6fb9478536
Simon Binet <binetsimon@gmail.com>
2023-07-27 14:19:51 +0200

    test  crontab sans succes

--- api/db/backup.sh
+++ api/db/backup.sh
@@ -3,14 +3,14 @@
 # Variables pour la connexion à la base de données
 DB_HOST="$POSTGRES_HOSTNAME"
 DB_USER="$POSTGRES_USER"
-DB_PASSWORD="$POSTGRES_PASSWORD"
 DB_NAME="$POSTGRES_DB"
 
 # Répertoire de sauvegarde
-BACKUP_DIR="/backups"  # Remplacez par le répertoire où vous souhaitez stocker les sauvegardes
+BACKUP_DIR="/usr/app/backups"  # Remplacez par le répertoire où vous souhaitez stocker les sauvegardes
 
 # Nom du fichier de sauvegarde
 BACKUP_FILENAME="db_backup_$(date +%Y%m%d_%H%M%S).sql"
 
 # Exécute la sauvegarde
-pg_dump -h "$DB_HOST" -U "$DB_USER" "$DB_NAME" > "$BACKUP_DIR/$BACKUP_FILENAME"
+PGPASSWORD="$POSTGRES_PASSWORD" pg_dump -h "$DB_HOST" -U "$DB_USER" "$DB_NAME" > "$BACKUP_DIR/$BACKUP_FILENAME"
+
